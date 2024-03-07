e540d6e53d78a63aea08be496688f49dd09b4cdf
Aries Powvalla <74034072+AriesNinjaDev@users.noreply.github.com>
2024-03-07 14:44:43 -0800

    2

--- src/cgl/Out.java
+++ src/cgl/Out.java
@@ -1,7 +1,33 @@
 package cgl;
 
-public class Out {
-    public static void main(String[] args) {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Out {
+
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    private static final Map<Boolean, String> charMap = new HashMap<>();
+
+    static {
+        charMap.put(true, ANSI_RED_BACKGROUND + "   " + ANSI_RESET);
+        charMap.put(false, "   ");
+    }
+
+    public static void displayContent(boolean[][] tiles) {
+        System.out.print(ANSI_RESET + "\n\n\n\n\n\n\n\n\n\n\n" + ANSI_BLUE_BACKGROUND);
+        for (int i = 0; i < tiles.length + 2; i++) System.out.print("   ");
+        System.out.print(ANSI_RESET + "\n" + ANSI_BLUE_BACKGROUND + "   " + ANSI_RESET);
+        for (boolean[] i : tiles) {
+            for (boolean j : i) {
+                System.out.print(charMap.get(j));
+            }
+            System.out.print(ANSI_BLUE_BACKGROUND + "   " + ANSI_RESET + "\n" + ANSI_BLUE_BACKGROUND + "   " + ANSI_RESET);
+        }
+        System.out.print(ANSI_BLUE_BACKGROUND);
+        for (int i = 0; i < tiles.length + 1; i++) System.out.print("   ");
+        System.out.print(ANSI_RESET);
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,6 +1,25 @@
+import cgl.Out;
+
+import java.util.Random;
+
 public class main {
+    public static boolean[][] tiles;
+
     public static void main(String[] args) {
 
-            
+        tiles = new boolean[9][9];
+
+        Random random = new Random();
+
+        // Fill the 2D array with random boolean values
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                tiles[i][j] = random.nextBoolean();
+            }
+        }
+
+        Out.displayContent(tiles);
+
     }
+
 }
